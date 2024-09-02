1ee9fb759ec709645675ef964c7a3ab68cf826e9
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2024-09-02 15:25:30 +0900

    5장 Promise 완료

---
+++ callback-promise-async-await/async-await.js
@@ -0,0 +1,17 @@
+
+function  waitOneSecond(msg) {
+    return new Promise((resolve, _) => {
+        setTimeout(() => resolve(`${msg}`), 1000);
+    });
+}
+
+async function countOneToTen() {    //10초 동안 1초마다 메시지 출력
+    for (let x of  [...Array(10).keys()]) { //0부터 9까지 루프를 순회
+        // 1초 대기 후 result에 결과값 저장
+        let result = await waitOneSecond(`${x + 1}초 대기 중...`);
+        console.log(result);
+    }
+    console.log("완료");
+}
+
+countOneToTen();
---
+++ callback-promise-async-await/top20-movie-async-await.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+async function getTop20Movies() {   // await를 사용하므로 async를 붙임
+    const url = "https://raw.githubusercontent.com/wapj/jsbackend/main/movieinfo.json";
+    try {
+        // 네트워크에서 데이터를 받아오므로 await로 기다림
+        const result = await axios.get(url);
+        const { data } = result;    // 결과값(result)에 data 프로퍼티가 있음
+        //data 또는 articleList 없을때 예외 처리
+        if(!data.articleList || data.articleList.size === 0) {
+            throw new Error("데이터가 없습니다!");
+        }
+        // data에서 필요한 영화 제목과 순위 정보를 뽑아냄
+        const movieInfos = data.articleList.map((article, idx) => {
+            return { title: article.title, rank: idx };
+        })
+
+        for (let movieInfo of movieInfos) {    // 받은 영화 리스트 정보 출력
+            console.log(`[${movieInfo.rank}위] ${movieInfo.title}`);
+        }
+    } catch (e) {
+        // 예외 처리는 기존코드와 같게 try catch로 감쌈
+        throw new Error(e);
+    }
+}
+
+// await를 함수 안에서만 사용 가능하므로 함수를 하나 생성해 실행
+getTop20Movies();
