66c2497ed576cc5d92baa593631259d47382ef5f
Park Na Jin <162278527+cosmoss919@users.noreply.github.com>
2024-09-27 09:53:30 +0900

    11장 구글 OAuth 로그인 구현 - Refresh Token이 오지 않음 수정해야함

--- Book/nest-auth-test/src/auth/auth.controller.ts
+++ Book/nest-auth-test/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post, Request, Response, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from "../user/user.dto";
 import { AuthService } from "./auth.service";
-import { AuthenticatedGuard, LocalAuthGuard, LoginGuard } from "./auth.guard";
+import { AuthenticatedGuard, LocalAuthGuard, LoginGuard, GoogleAuthGuard } from "./auth.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -60,5 +60,16 @@ export class AuthController {
     @Get('test-guard2')
     testGuardWithSession(@Request() req) {
         return req.user;
+    }
+
+    @Get('to-google')   // 구글 로그인으로 이동하는 라우터 메서드
+    @UseGuards(GoogleAuthGuard)
+    async googleAuth(@Request() req){}
+
+    @Get('google')  //구글 로그인 후 콜백 실행 후 콜백 실행 후 이동 시 실행되는 라이터 메서드
+    @UseGuards(GoogleAuthGuard)
+    async googleAuthRedirect(@Request() req, @Response() res){
+        const { user } = req;
+        return res.send(user);
     }
 }
--- Book/nest-auth-test/src/auth/auth.guard.ts
+++ Book/nest-auth-test/src/auth/auth.guard.ts
@@ -52,3 +52,14 @@ export class AuthenticatedGuard implements CanActivate {
         return request.isAuthenticated();   //세션에서 정보를 읽어서 인증 확인
     }
 }
+
+@Injectable()
+export class GoogleAuthGuard extends AuthGuard('google') {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;   // super.canActive에서 stategy의 validate함수를 실행 => 성공하면 serializer 실행
+        // 컨텍스트에서 리퀘스트 객체를 꺼냄
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request); //세션 저장
+        return result;
+    }
+}
